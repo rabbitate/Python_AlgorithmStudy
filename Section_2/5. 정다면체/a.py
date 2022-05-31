f2c1703fad87c3b98b7fed53fe46468ef67ee71b
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2022-05-31 16:44:23 +0900

    정다면체 문제 풀이

---
+++ Section_2/5. 정다면체/a.py
@@ -0,0 +1,25 @@
+# 두 개의 정 N면체와 정 M면체의 두 개의 주사위를 던져서 나올 수 있는 눈의 합 중 가장 확 률이 높은 숫자를 출력하는 프로그램을 작성하세요.
+# 정답이 여러 개일 경우 오름차순으로 출력합니다.
+
+# 첫 번째 줄에는 자연수 N과 M이 주어집니다. N과 M은 4, 6, 8, 12, 20 중의 하나입니다.
+
+import sys
+# sys.stdin = open("in1.txt", 'r')
+
+n, m = map(int, input().split())
+
+cnt = [0] * (n + m + 5) # 주사위 수의 합 개수를 세주는 리스트, 넉넉하게 크기 설정 후 0으로 초기화 해주었다
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        cnt[i+j] += 1
+
+max = 2 # 1+1
+
+for index in range(len(cnt)):
+    if max < cnt[index]:
+        max = cnt[index] # 최대값 갱신
+
+for index in range(len(cnt)):
+    if cnt[index] == max:
+        print(index, end=' ')
