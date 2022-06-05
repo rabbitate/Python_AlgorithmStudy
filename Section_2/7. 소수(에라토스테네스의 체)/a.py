007326dde75f19e8037fdf60ceacd48a84a38779
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2022-06-05 18:51:42 +0900

    Update a.py
    Count_decimal(n) -> Count_prime(n) 함수의 역할에 맞게 함수명 변경

--- Section_2/7. 소수(에라토스테네스의 체)/a.py
+++ Section_2/7. 소수(에라토스테네스의 체)/a.py
@@ -5,19 +5,19 @@ import sys
 
 # # 1부터 n까지의 소수의 개수 리턴해주는 함수(재귀로 구현)
 
-# def Count_decimal(n):
+# def Count_prime(n):
 #     if n == 2: return 1 # 2일 경우 1 리턴(2는 소수)
 #     else:
 #         for i in range(2,n): # 소수인지 검증
 #             if n % i == 0: # 소수가 아니라면
-#                 return Count_decimal(n-1) # 1을 더해주지 않고 재귀 호출
-#         else: return 1 + Count_decimal(n-1) # n이 소수라면 개수에 1을 더해주고 재귀 호출
+#                 return Count_prime(n-1) # 1을 더해주지 않고 재귀 호출
+#         else: return 1 + Count_prime(n-1) # n이 소수라면 개수에 1을 더해주고 재귀 호출
 
 # # sys.stdin = open("in5.txt", 'r')
 
 n = int(input())
 
-# print(Count_decimal(n))
+# print(Count_prime(n))
 
 list = [0] * (n+1) # n+1 크기의 리스트를 모두 0으로 초기화
 cnt = 0 # 1부터 n까지 소수의 개수 변수
