3982e93175620d17557e2b27e27d494000a08f03
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2022-06-05 18:24:47 +0900

    소수(에라토스테네스의 체) 문제 풀이
    재귀를 활용하여 해결하려 했으니 n이 1000이상일 경우 재귀 깊이를 넘게 되어 에라토스테네스의 체를 활용하여 해결하였습니다.

--- Section_2/7. 소수(에라토스테네스의 체)/a.py
+++ Section_2/7. 소수(에라토스테네스의 체)/a.py
@@ -3,18 +3,29 @@
 
 import sys
 
-# 1부터 n까지의 소수의 개수 리턴해주는 함수(재귀로 구현)
+# # 1부터 n까지의 소수의 개수 리턴해주는 함수(재귀로 구현)
 
-def Count_decimal(n):
-    if n == 2: return 1 # 2일 경우 1 리턴(2는 소수)
-    else:
-        for i in range(2,n): # 소수인지 검증
-            if n % i == 0: # 소수가 아니라면
-                return Count_decimal(n-1) # 1을 더해주지 않고 재귀 호출
-        else: return 1 + Count_decimal(n-1) # n이 소수라면 개수에 1을 더해주고 재귀 호출
+# def Count_decimal(n):
+#     if n == 2: return 1 # 2일 경우 1 리턴(2는 소수)
+#     else:
+#         for i in range(2,n): # 소수인지 검증
+#             if n % i == 0: # 소수가 아니라면
+#                 return Count_decimal(n-1) # 1을 더해주지 않고 재귀 호출
+#         else: return 1 + Count_decimal(n-1) # n이 소수라면 개수에 1을 더해주고 재귀 호출
 
-# sys.stdin = open("in5.txt", 'r')
+# # sys.stdin = open("in5.txt", 'r')
 
 n = int(input())
 
-print(Count_decimal(n))
+# print(Count_decimal(n))
+
+list = [0] * (n+1) # n+1 크기의 리스트를 모두 0으로 초기화
+cnt = 0 # 1부터 n까지 소수의 개수 변수
+
+for i in range(2,n+1):
+    if list[i] == 0: # 리스트의 값이 0일때 (소수라면)
+        cnt += 1 # 1 증가
+        for j in range(i,n+1,i): # i부터 n까지의 숫자 중 i의 배수는
+            list[j] = 1 # 소수가 아니다 (걸러짐)
+
+print(cnt)
