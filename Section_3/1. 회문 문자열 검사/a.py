38f12eed10436e04cf3e076dffc15837032a8b01
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2022-06-12 15:52:28 +0900

    Update a.py
    모두 대문자로 변경 후 슬라이싱을 활용해 회문인지 확인하는 다른 방식의 알고리즘으로 해결

--- Section_3/1. 회문 문자열 검사/a.py
+++ Section_3/1. 회문 문자열 검사/a.py
@@ -6,12 +6,19 @@ import sys
 dif = abs(ord('a')-ord('A')) # 같은 알파벳이지만 대소문자가 다를 경우의 아스키 코드 차이값
 
 def check(s):
-    for i in range(len(s)//2): # 문자열의 절반만 검사
-        if s[i] != s[-1-i] and abs(ord(s[i]) - ord(s[-1-i])) != dif: # 같은 문자가 아니고, 대소문자 구분 없이 같은 문자가 아니라면
-            print("NO") # 회문 문자열이 아니다
-            break
-    else:
+    # for i in range(len(s)//2): # 문자열의 절반만 검사
+    #     if s[i] != s[-1-i] and abs(ord(s[i]) - ord(s[-1-i])) != dif: # 같은 문자가 아니고, 대소문자 구분 없이 같은 문자가 아니라면
+    #         print("NO") # 회문 문자열이 아니다
+    #         break
+    # else:
+    #     print("YES")
+
+    s = s.upper() # 대소문자 구별이 없으므로 모두 대문자로 변경 후
+
+    if s == s[::-1]: # 회문의 슬라이싱과 같을 경우
         print("YES")
+    else:
+        print("NO")
 
 sys.stdin = open("in5.txt", 'r')
 
